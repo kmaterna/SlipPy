0ca331d6717c180e22fe32f1dc4b2ead14548371
kmaterna <kathrynmaterna@gmail.com>
2020-05-05 14:14:29 -0700

    adding exceptions and adding vector scale to gmt plot

--- exec/gmt/gmt_plot.sh
+++ exec/gmt/gmt_plot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Script to plot the fault patches with components of slip, 
+# such as strike slip, dip slip, or tensile. 
 
 slip_file=$1
 title=$2
@@ -10,8 +12,9 @@ proj=$7
 cmin=$8
 cmax=$9
 cntv=${10}
-predicted_gps_file=${11}
-observed_gps_file=${12}
+observed_gps_file=${11}
+predicted_gps_file=${12}
+
 
 range=$ll1/$ur1/$ll2/$ur2
 output=$title.ps
@@ -25,11 +28,10 @@ horiz_scale=10
 awk '{print $1, $2, $3, $4, $6, $7, 0}' $predicted_gps_file | gmt psvelo -A+e+gblue+pthickest -Wblue -Se$horiz_scale/0.68/10 -R$range -J$proj -O -K >> $output
 awk '{print $1, $2, $3, $4, $6, $7, 0}'  $observed_gps_file | gmt psvelo  -A+e+gred+pthickest -Wred -Se$horiz_scale/0.68/10 -R$range -J$proj -O -K >> $output
 
-
 gmt psscale -Cmycpt.cpt -Dx7c/-1c+w12c/0.5c+jTC+h -Bxaf+l$title" slip" -By+l"m" -K -O >> $output
+
 
 rm gmt.history 
 rm mycpt.cpt
-
 open $output
 
--- exec/gmt/quiver_plot.sh
+++ exec/gmt/quiver_plot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# GMT plot to show the GPS inversion's fit to the data. 
 
-predicted_gps_file=$1
-observed_gps_file=$2
+observed_gps_file=$1
+predicted_gps_file=$2
 ll1=$3
 ll2=$4
 ur1=$5
@@ -14,8 +15,15 @@ horiz_scale=10
 
 gmt pscoast -R$range -J$proj -Wthin,black -Di -N1 -N2 -B1.0 -B+t"GPS displacements" -Y4 -Gwhite -Slightgray -K > $output
 awk '{print $1, $2, $3, $4, $6, $7, 0}' $predicted_gps_file | gmt psvelo -A+e+gblue+pthickest -Wblue -Se$horiz_scale/0.68/10 -R$range -J$proj -O -K >> $output
-awk '{print $1, $2, $3, $4, $6, $7, 0}'  $observed_gps_file | gmt psvelo  -A+e+gred+pthickest -Wred -Se$horiz_scale/0.68/10 -R$range -J$proj -O -K >> $output
+awk '{print $1, $2, $3, $4, $6, $7, 0}'  $observed_gps_file | gmt psvelo -A+e+gred+pthickest -Wred -Se$horiz_scale/0.68/10 -R$range -J$proj -O -K >> $output
+
+# Scale vectors. Their placement and length will be problem-dependent. Might need to adjust. 
+gmt psvelo -A+e+gblue+pthickest -Wblue -Se$horiz_scale/0.68/10 -R$range -J$proj -O -K <<EOF>> $output
+`echo $ll1 + 1.2 | bc` `echo $ll2 + 0.2 | bc` .050 0 0 0 0 5cm model
+EOF
+gmt psvelo -A+e+gred+pthickest -Wred -Se$horiz_scale/0.68/10 -R$range -J$proj -O -K <<EOF>> $output
+`echo $ll1 + 1.2 | bc` `echo $ll2 + 0.3 | bc` .050 0 0.003 0.003 0 5cm obs
+EOF
 
 rm gmt.history 
-
 open $output
