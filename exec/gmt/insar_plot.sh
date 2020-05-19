9373fc2b78e93bfc210af36ff6f3605b141d06d5
kmaterna <kathrynmaterna@gmail.com>
2020-05-18 21:42:57 -0700

    small gmt plotting changes

--- exec/gmt/insar_plot.sh
+++ exec/gmt/insar_plot.sh
@@ -13,7 +13,8 @@ ur2=$6
 proj=$7
 scale_low=$8
 scale_high=$9
-scaele_int=$10
+scale_int=${10}
+slip_file=${11}
 
 range=$ll1/$ur1/$ll2/$ur2
 output="insar_displacements.ps"
@@ -33,6 +34,7 @@ awk '{print $1, $2, $3*1000}' $model_insar_file | gmt psxy -Sc0.04 -Cmycpt.cpt -
 gmt pscoast -R$range -J$proj -Wthin,black -Di -N1 -N2 -B1.0 -B+t"Residual" -X3.5i -Gwhite -Slightgray -O -K >> $output
 paste $obs_insar_file $model_insar_file | awk '{print $1, $2, $3*1000, $10*1000}'  > temp_insar.txt
 awk '{print $1, $2, $3-$4}' temp_insar.txt | gmt psxy -Sc0.04 -Cmycpt.cpt -R$range -J$proj -O -K >> $output
+gmt psxy $slip_file -R$range -J$proj -Wthinnest,gray -L -K -O >> $output # Putting the faults on there for kicks
 
 gmt psscale -Cmycpt.cpt -Dx-7c/-1c+w9c/0.5c+jTC+h -Bxaf+l"Disp" -By+l"mm" -K -O >> $output
 
